299d367fc94621640a1b1df6904c728df30f2bcb
vm <mailvm12@gmail.com>
2025-01-03 19:51:38 +0300

    add test

--- cmd/main.go
+++ cmd/main.go
@@ -15,52 +15,6 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func main() {
-	// Подлючение к БД
-	dbConn, err := db.Connect("root", "rootpassword", "localhost", 3306, "currency_db")
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-	defer dbConn.Close()
-
-	// Создание таблицы
-	createTableQuery := `
-        CREATE TABLE IF NOT EXISTS currency_rates (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            cur_id INT NOT NULL,
-            date DATE NOT NULL,
-            cur_abbreviation VARCHAR(10) NOT NULL,
-            cur_scale INT NOT NULL,
-            cur_name VARCHAR(100) NOT NULL,
-            cur_official_rate DECIMAL(10,4) NOT NULL
-        )
-    `
-	_, err = dbConn.Exec(createTableQuery)
-	if err != nil {
-		log.Fatalf("Failed to create table: %v", err)
-	}
-
-	// Создание маршрутов
-	r := mux.NewRouter()
-
-	// Создание обработчика
-	h := &handlers.Handler{DB: dbConn}
-
-	// GET http://localhost:8080/rates вернёт все собранные записи.
-	r.HandleFunc("/rates", h.GetAllRatesHandler).Methods("GET")
-
-	// GET http://localhost:8080/rates/day?date=2025-01-03 вернёт записи на конкретный день (формат YYYY-MM-DD)
-	r.HandleFunc("/rates/day", h.GetRatesByDateHandler).Methods("GET")
-
-	// StartScheduler(dbConn) запускает планировщик, который каждый день в 01:00 собирает курсы валют
-
-	// Старт сервера
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal("Failed to start server:", err)
-	}
-}
-
 func StartScheduler(db *sql.DB) {
 	c := cron.New()
 	// Каждый день в 01:00
@@ -98,4 +52,80 @@ func StartScheduler(db *sql.DB) {
 		log.Println("Rates successfully inserted for date:", time.Now())
 	})
 	c.Start()
+}
+
+func fetchAndInsert(dbConn *sql.DB) error {
+	rates, err := service.FetchRatesFromNBRB()
+	if err != nil {
+		return err
+	}
+
+	var toInsert []repository.CurrencyRate
+	for _, r := range rates {
+		dateParsed, err := time.Parse("2006-01-02T15:04:05", r.Date)
+		if err != nil {
+			return err
+		}
+		toInsert = append(toInsert, repository.CurrencyRate{
+			CurID:           r.CurID,
+			Date:            dateParsed,
+			CurAbbreviation: r.CurAbbreviation,
+			CurScale:        r.CurScale,
+			CurName:         r.CurName,
+			CurOfficialRate: r.CurOfficialRate,
+		})
+	}
+
+	return repository.InsertCurrencyRates(dbConn, toInsert)
+}
+
+func main() {
+	// Подлючение к БД
+	dbConn, err := db.Connect("root", "root", "127.0.0.1", 3306, "currency_db")
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer dbConn.Close()
+
+	// Создание таблицы
+	createTableQuery := `
+        CREATE TABLE IF NOT EXISTS currency_rates (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            cur_id INT NOT NULL,
+            date DATE NOT NULL,
+            cur_abbreviation VARCHAR(10) NOT NULL,
+            cur_scale INT NOT NULL,
+            cur_name VARCHAR(100) NOT NULL,
+            cur_official_rate DECIMAL(10,4) NOT NULL
+        )
+    `
+	_, err = dbConn.Exec(createTableQuery)
+	if err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
+
+	// Создание маршрутов
+	r := mux.NewRouter()
+
+	// Создание обработчика
+	h := &handlers.Handler{DB: dbConn}
+
+	// GET http://localhost:8080/rates вернёт все собранные записи.
+	r.HandleFunc("/rates", h.GetAllRatesHandler).Methods("GET")
+
+	// GET http://localhost:8080/rates/day?date=2025-01-03 вернёт записи на конкретный день (формат YYYY-MM-DD)
+	r.HandleFunc("/rates/day", h.GetRatesByDateHandler).Methods("GET")
+
+	// StartScheduler(dbConn) // запускает планировщик, который каждый день в 01:00 собирает курсы валют
+
+	// Тестовый запуск
+	if err := fetchAndInsert(dbConn); err != nil {
+		log.Fatal(err)
+	}
+
+	// Старт сервера
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
